b433be56a940982120a772a39e8495542a2c10c2
K1 <K1@DESKTOP-VJ74ISC>
2023-05-04 00:48:37 +0430

    Add project files.

---
+++ SerializingAsXML/Person.cs
@@ -0,0 +1,19 @@
+﻿namespace SerializingAsXML;
+
+public class Person
+{
+    public Person()
+    {
+    }
+
+    public Person(decimal salary)
+    {
+        Salary = salary;
+    }
+
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public HashSet<Person>? Children { get; set; }
+    public decimal Salary { get; set; }
+}
---
+++ SerializingAsXML/Program.cs
@@ -0,0 +1,70 @@
+﻿using SerializingAsXML;
+using System.Xml.Serialization;
+using static System.Console;
+using static System.Environment;
+using static System.IO.Path;
+
+// create an object graph
+List<Person> people = new()
+{
+    new(30000M)
+    {
+        FirstName = "Keyvan",
+        LastName = "Parvizinia",
+        DateOfBirth = new(1990, 10, 26)
+    },
+    new(40000M)
+    {
+        FirstName = "Saman",
+        LastName = "Mohammadi",
+        DateOfBirth = new(1969, 11, 23)
+    },
+    new(20000M)
+    {
+        FirstName = "Reza",
+        LastName = "Ahmadi",
+        DateOfBirth = new(1984, 5, 4),
+        Children = new()
+        {
+            new(0M)
+            {
+                FirstName = "Arwin",
+                LastName = "Ahmadi",
+                DateOfBirth = new(2000, 7, 12)
+            }
+        }
+    }
+};
+
+// create object that will format a List of Persons as XML
+XmlSerializer xs = new(people.GetType());
+
+// create a file to write to
+string path = Combine(GetFolderPath(SpecialFolder.Personal), "people.xml");
+
+using (FileStream stream = File.Create(path))
+{
+    // serialize the object graph to the stream
+    xs.Serialize(stream, people);
+}
+
+WriteLine($"Written {new FileInfo(path).Length} bytes of XML to {path}");
+WriteLine();
+
+// Display the serialized object graph
+WriteLine(File.ReadAllText(path));
+
+//Deserializing XML files
+using (FileStream xmlLoad = File.Open(path, FileMode.Open))
+{
+    // deserialize and cast the object graph into a List of Person
+    List<Person>? loadedPeople = xs.Deserialize(xmlLoad) as List<Person>;
+
+    if (loadedPeople is not null)
+    {
+        foreach (Person p in loadedPeople)
+        {
+            WriteLine($"{p.LastName} has {p.Children?.Count ?? 0} children.");
+        }
+    }
+}
